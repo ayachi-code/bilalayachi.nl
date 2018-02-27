f1d9ab23ef4b9e2edbffc262bf5825c5c977e524
Bilal ayachi <bilal.ayachi@hetmml.nl>
2018-02-27 19:30:10 +0100

    test

---
+++ public/index.js
@@ -0,0 +1,21 @@
+function progressBar(elem, einde) {
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= einde) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+    }
+  }
+}
+
+progressBar(document.getElementById('myBar'), 80);
+progressBar(document.getElementById('myBar1'), 60);
+progressBar(document.getElementById('myBar2'), 75);
+progressBar(document.getElementById('myBar3'), 75);
+progressBar(document.getElementById('myBar4'), 55);
+progressBar(document.getElementById('myBar5'), 65);
+progressBar(document.getElementById('myBar6'), 60);
+progressBar(document.getElementById('myBar7'), 68)
